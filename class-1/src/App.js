4655b9c4838f81a9a6d9d87fe16c841b10681cdf
ASHUTOSH RAJ <ashutoshraj0420@gmail.com>
2022-10-06 16:13:03 +0530

    add

--- class-1/src/App.js
+++ class-1/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Counter from './componenets/Counter';
-
+import {useState} from 'react'
 function App() {
+  const [showCounter, setShowCounter]= useState(true)
   return (
     <div className="App">
-     < Counter />
+    {showCounter && < Counter />}
+    <button onClick={()=>setShowCounter((prev)=>!prev)}>Toggle</button>
 
     </div>
   );
--- class-1/src/componenets/Counter.jsx
+++ class-1/src/componenets/Counter.jsx
@@ -2,10 +2,28 @@ import React, {useState,useEffect} from 'react'
 
 const Counter = () => {
     const [count, setCount] = useState(0)
-
+    console.log("1")
     useEffect(()=>{
+        console.log("2")
+        return ()=>{
+            console.log("3")
+        }
+    },[count])
+    useEffect(()=>{
+        console.log("4")
+        return ()=>{
+            console.log("5") 
+        }
+    },[])
+    useEffect(()=>{
+        console.log("6")
+        return ()=>{
+            console.log("7")
+            
+        }
+    }, [count])
+    console.log("8")
 
-    }, [])
 
     return (
         <div>
